78b80e7aa6680ff937e98147fe2b8198dd05d146
AtifKhan4168 <147045322+AtifKhan4168@users.noreply.github.com>
2023-10-06 17:04:21 +0500

    assignment1 (3 files)

---
+++ 10 methods.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Atif_10_Mthods
+{
+    internal class Program
+    {
+        
+        // Method 1: Division 
+        public static int Divide(int a, int b)
+        {
+            if (b != 0)
+            {
+                return a / b;
+            }
+
+            Console.WriteLine("Division by zero is restricted.");
+            return 0;
+        }
+        
+        // Method 2: Multiplication
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        
+        // Method 3: Addition
+        public static int Add(int a, int b)
+        {
+            return a + b; 
+        }
+
+       // Method 4: Subtraction
+       public static int Subtract(int a, int b)
+       {
+           return a - b;
+       }
+       
+       // Method 5: Odd number
+       public static bool OddorNot(int num)
+       {
+           return num / 2 != 0;
+       }
+
+       // Method 6: Even number
+       public static bool EvenorNot(int num)
+       {
+        return num / 2 == 0;
+       }
+       
+       // Method 7: Sentence
+       public static void Statement(string name)
+       {
+           Console.WriteLine("My name is " + name + "!");
+       }
+
+       // Method 8: Prime number
+       public static bool IsPrime(int num)
+       {
+        if (num <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(num); i++)
+        {
+            if (num / i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+       }
+
+       // Method 9: Taking Square
+       public static int Square(int num)
+       {
+        return num * num;
+       }
+
+       // Method 10: Taking Cube
+       public static int Cube(int num)
+       {
+        return num * num * num;
+       }
+
+       
+       public static void Main()
+       { 
+        Console.WriteLine("Division: " + Divide(50, 5));
+        Console.WriteLine("Multiplication: " + Multiply(3, 8));
+        Console.WriteLine("Addition: " + Add(6, 2));
+        Console.WriteLine("Subtraction: " + Subtract(5, 1));
+        Console.WriteLine("Is 5 odd? " + OddorNot(4));
+        Console.WriteLine("Is 3 even? " + EvenorNot(3));
+        Statement("Atif Sharif");
+        Console.WriteLine("Is 7 prime? " + IsPrime(7));
+        Console.WriteLine("Square of 2: " + Square(2));
+        Console.WriteLine("Cube of 2: " + Cube(2));
+        
+       } 
+    }
+}
---
+++ 5 data types 10 variabls.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace Atif_5_data_types_10_variables
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name1 = "Sarwar";
+            string name2 = "Usama";
+            string name3 = "Hammad";
+            string name4 = "Babar";
+            string name5 = "Hassan";
+            string name6 = "Fahad";
+            string name7 = "Danish";
+            string name8 = "Shujja";
+            string name9 = "Hashim";
+            string name10 = "Daniyal";
+            
+            int age1 = 24;
+            int age2 = 23;
+            int age3 = 22;
+            int age4 = 21;
+            int age5 = 20;
+            int age6 = 19;
+            int age7 = 18;
+            int age8 = 17;
+            int age9 = 16;
+            int age10 = 15;
+            
+            float average1 = 3.5f;
+            float average2 = 3.4f;
+            float average3 = 3.3f;
+            float average4 = 2.8f;
+            float average5 = 2.7f;
+            float average6 = 2.6f;
+            float average7 = 2.5f;
+            float average8 = 1.9f;
+            float average9 = 1.8f;
+            float average10 = 1.7f;
+            
+            bool pass1 = true;
+            bool pass2 = true;
+            bool pass3 = true;
+            bool pass4 = true;
+            bool pass5 = true;
+            bool pass6 = true;
+            bool pass7 = true;
+            bool pass8 = false;
+            bool pass9 = false;
+            bool pass10 = false;
+            
+            char grade1 = 'a';
+            char grade2 = 'a';
+            char grade3 = 'a';
+            char grade4 = 'a';
+            char grade5 = 'a';
+            char grade6 = 'b';
+            char grade7 = 'b';
+            char grade8 = 'c';
+            char grade9 = 'c';
+            char grade10 = 'c';
+            
+            
+            Console.WriteLine("Name: " + name1);
+            Console.WriteLine("Name: " + name2);
+            Console.WriteLine("Name: " + name3);
+            Console.WriteLine("Name: " + name4);
+            Console.WriteLine("Name: " + name5);
+            Console.WriteLine("Name: " + name6);
+            Console.WriteLine("Name: " + name7);
+            Console.WriteLine("Name: " + name8);
+            Console.WriteLine("Name: " + name9);
+            Console.WriteLine("Name: " + name10);
+                                          
+            
+            
+            Console.WriteLine("Age: " + age1);
+            Console.WriteLine("Age: " + age2);
+            Console.WriteLine("Age: " + age3);
+            Console.WriteLine("Age: " + age4);
+            Console.WriteLine("Age: " + age5);
+            Console.WriteLine("Age: " + age6);
+            Console.WriteLine("Age: " + age7);
+            Console.WriteLine("Age: " + age8);
+            Console.WriteLine("Age: " + age9);
+            Console.WriteLine("Age: " + age10);
+            
+            
+            
+            Console.WriteLine("Average: " + average1);
+            Console.WriteLine("Average: " + average2);
+            Console.WriteLine("Average: " + average3);
+            Console.WriteLine("Average: " + average4);
+            Console.WriteLine("Average: " + average5);
+            Console.WriteLine("Average: " + average6);
+            Console.WriteLine("Average: " + average7);
+            Console.WriteLine("Average: " + average8);
+            Console.WriteLine("Average: " + average9);
+            Console.WriteLine("Average: " + average10);
+            
+            
+            
+            Console.WriteLine("Pass: " + true);
+            Console.WriteLine("Pass: " + true);
+            Console.WriteLine("Pass: " + true);
+            Console.WriteLine("Pass: " + true);
+            Console.WriteLine("Pass: " + true);
+            Console.WriteLine("Pass: " + true);
+            Console.WriteLine("Pass: " + true);
+            Console.WriteLine("Pass: " + false);
+            Console.WriteLine("Pass: " + false);
+            Console.WriteLine("Pass: " + false);
+            
+            
+            
+            Console.WriteLine("Grade: " + 'a');
+            Console.WriteLine("Grade: " + 'a');
+            Console.WriteLine("Grade: " + 'a');
+            Console.WriteLine("Grade: " + 'a');
+            Console.WriteLine("Grade: " + 'a');
+            Console.WriteLine("Grade: " + 'b');
+            Console.WriteLine("Grade: " + 'b');
+            Console.WriteLine("Grade: " + 'c');
+            Console.WriteLine("Grade: " + 'c');
+            Console.WriteLine("Grade: " + 'c');
+            
+            Console.WriteLine();
+            
+        }
+    }
+}
